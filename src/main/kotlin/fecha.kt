c8150f3061e6dce1a62317df51a6f9a02c52f217
Manuel Santos <manuel.sant36@gmail.com>
2024-03-12 23:10:20 +0100

    refactorizacion final

--- src/main/kotlin/fecha.kt
+++ src/main/kotlin/fecha.kt
@@ -1,18 +1,40 @@
-class Fch(private val d: Int, private val m: Int, private val a: Int) {
+/**
+ * Fecha
+ *
+ * @property dia
+ * @property mes
+ * @property anio
+ * @constructor Create empty Fecha
+ */
+class Fecha(private val dia: Int, private val mes: Int, private val anio: Int) {
 
 
     fun valida(): Boolean {
 
-        if (d < 1 || d > 31) return false
-        if (m < 1 || m > 12) return false
+        var esValida = false
+        if ((dia >= 1 || dia <= 31) && (mes >= 1 || mes <= 12)) {
+            var diasMes = diasDelMes(mes)
+            esValida = dia <= diasMes
+
+        }
 
         // determinamos la cantidad de días del mes:
+
+        return esValida
+    } // … más métodos
+
+    private fun esBisiesto(anio: Int): Boolean = anio % 400 == 0 || anio % 4 == 0 && anio % 100 != 0
+
+
+    private fun diasDelMes(mes: Int): Int {
         var diasMes = 0
-        when (m) {
+        when (mes) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
             4, 6, 9, 11 -> diasMes = 30
-            2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
+            2 -> diasMes = if (esBisiesto(anio)) 29 else 28
         }
-        return if (d > diasMes) false else true
-    } // … más métodos
+        return diasMes
+    }
 }
+
+
