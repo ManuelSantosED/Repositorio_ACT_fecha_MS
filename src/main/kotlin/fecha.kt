7cb11efac51a850bccbef46a5867f2bfd2122f98
Manuel Santos <manuel.sant36@gmail.com>
2024-03-12 14:06:53 +0100

    Comprobado Historial

--- src/main/kotlin/fecha.kt
+++ src/main/kotlin/fecha.kt
@@ -1,29 +1,18 @@
-
-    class Fecha(private val dias: Int, private val mes: Int, private val anio: Int) {
-
-
-        fun valida(): Boolean {
-
-            if (dias < 1 || dias > 31) return false
-            if (mes < 1 || mes > 12) return false
-
-            // determinamos la cantidad de días del mes:
-            return diasMes()
-        } // … más métodos
+class Fch(private val d: Int, private val m: Int, private val a: Int) {
 
 
+    fun valida(): Boolean {
 
+        if (d < 1 || d > 31) return false
+        if (m < 1 || m > 12) return false
 
-    private fun diasMes(): Boolean {
+        // determinamos la cantidad de días del mes:
         var diasMes = 0
-        when (mes) {
+        when (m) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
             4, 6, 9, 11 -> diasMes = 30
-            2 -> diasMes = if (anio % 400 == 0 || anio % 4 == 0 && anio % 100 != 0) 29 else 28
+            2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
         }
-        return if (dias > diasMes) false else true
-    }
+        return if (d > diasMes) false else true
+    } // … más métodos
 }
-
-
-
